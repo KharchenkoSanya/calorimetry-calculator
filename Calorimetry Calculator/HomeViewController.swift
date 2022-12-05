b7c3efb8fa816e360aa9e9d058956534719a48f1
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-05 17:57:02 +0200

    Completed app

--- Calorimetry Calculator/HomeViewController.swift
+++ Calorimetry Calculator/HomeViewController.swift
@@ -22,9 +22,68 @@ class HomeViewController: UIViewController {
         weightField.becomeFirstResponder()
     }
     
-    @IBAction func calculateDidTap(_ sender: Any) {}
+    @IBAction func calculateDidTap(_ sender: Any) {
+        guard let weight = Int(weightField.text ?? ""),
+              let height = Int(heightField.text ?? ""),
+              let age = Int(ageField.text ?? ""),
+              weight > 0 && height > 0 && age > 0 else {
+            return
+        }
+        
+        let activityIndex = pickerView.selectedRow(inComponent: 0)
+        let activity = activities[activityIndex]
+        
+        var activityValue = 0
+        switch activity {
+        case "None":
+            activityValue = 0
+        case "Low":
+            activityValue = 50
+        case "Medium":
+            activityValue = 150
+        case "High":
+            activityValue = 250
+        default:
+            activityValue = 0
+        }
+
+        let seletedSex = sexSegmentControl.selectedSegmentIndex
+
+        switch seletedSex {
+        case 0:
+            let result = Double(10 * weight) + (6.25 * Double(height)) - Double(5 * age) + 5.0 + Double(activityValue)
+            showAlertWith(title: String(result))
+            
+        case 1:
+            let result = Double(8 * weight) + (5.25 * Double(height)) - Double(5 * age) + 5.0 - 161.0 + Double(activityValue)
+            showAlertWith(title: String(result))
+
+        default: ()
+        }
+    }
     
-    @IBAction func clearDidTap(_ sender: Any) {}
+    func showAlertWith(title: String) {
+        let alert = UIAlertController(title: "Your result", message: title, preferredStyle: .alert)
+        alert.addAction(.init(title: "Ok", style: .cancel))
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func sexControlDidChange(_ sender: UISegmentedControl) {
+        clear()
+    }
+    
+    @IBAction func clearDidTap(_ sender: Any) {
+        clear()
+    }
+    
+    func clear() {
+        weightField.text = nil
+        heightField.text = nil
+        ageField.text = nil
+        pickerView.selectRow(0, inComponent: 0, animated: true)
+        selectActivityBy(row: 0)
+        weightField.becomeFirstResponder()
+    }
     
     func configureSexSegmentControl() {
         sexSegmentControl.removeAllSegments()
